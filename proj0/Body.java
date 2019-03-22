2672b252fdf225eb6ab837209797caf986a71de7
1stdayofrest <247848502@qq.com>
2019-03-22 10:26:50 +0800

    modified proj0 NBody problem 2

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,13 +1,13 @@
 
 
 public class Body  {
-	private double xxPos;
-	private double yyPos;
-	private double xxVel;
-	private double yyVel;
-	private double mass;
-	private String imgFileName;
-	private double G = 6.67e-11;
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+	public double G = 6.67e-11;
 	public Body(double xP, double yP, double xV,
 				 double yV, double m, String img){
 		xxPos = xP;
