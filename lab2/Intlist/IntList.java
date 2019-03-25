43490d19e08c8c22d2dd57e428bfce877b2ab661
1stdayofrest <247848502@qq.com>
2019-03-25 16:05:49 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,17 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null){
+            return B;
+        }
+        IntList curA = A;
+        while (curA.rest != null){
+            curA = curA.rest;
+        }
+        curA.rest = B;
+
+        return A;
+
     }
 
     /**
@@ -91,7 +101,18 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null){
+            return B;
+        }
+        IntList C = new IntList(A.first, null);
+        IntList curC = C;
+        while (A.rest != null){
+            A = A.rest;
+            curC.rest = new IntList(A.first, null);
+            curC = curC.rest;
+        }
+        curC.rest = B;
+        return C;
     }
 
 
